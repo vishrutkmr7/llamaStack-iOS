1c1444649026ea5ee9cd42fd9ca2253479a2ca2d
Vishrut Jha <me@vishrutjha.com>
2024-12-17 01:03:03 -0700

    Initial Commit

---
+++ llamaStak-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  llamaStak-ios
+//
+//  Created by Vishrut Jha on 12/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ llamaStak-ios/llamaStak_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  llamaStak_iosApp.swift
+//  llamaStak-ios
+//
+//  Created by Vishrut Jha on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct llamaStak_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
